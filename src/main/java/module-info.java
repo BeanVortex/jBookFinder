0ce18e2c3ed80f9913423df726db95aeb9fcc01f
DarkDeveloper_Plant <goldeagle00110011@outlook.com>
2023-01-10 21:44:10 +0330

    - dep update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,11 @@ module ir.darkdeveloper.jbookfinder {
     requires java.logging;
     requires java.sql;
     requires reactor.core;
-    requires commons.io;
     requires org.controlsfx.controls;
     requires org.jsoup;
     requires java.desktop;
     requires jdk.crypto.cryptoki;
+    requires org.apache.commons.io;
 
     opens ir.darkdeveloper.jbookfinder to javafx.fxml;
     opens ir.darkdeveloper.jbookfinder.controllers to javafx.fxml;
